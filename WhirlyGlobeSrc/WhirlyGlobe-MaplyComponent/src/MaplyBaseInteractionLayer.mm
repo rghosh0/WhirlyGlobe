1b4a68cdfbc276d115b86fa224e664217fbacc5d
Ranen Ghosh <ranen@ranenghosh.com>
2014-12-05 19:51:59 -0500

    marker rotation changes

--- WhirlyGlobeSrc/WhirlyGlobe-MaplyComponent/src/MaplyBaseInteractionLayer.mm
+++ WhirlyGlobeSrc/WhirlyGlobe-MaplyComponent/src/MaplyBaseInteractionLayer.mm
@@ -594,11 +594,8 @@ typedef std::set<ThreadChanges> ThreadChangeSet;
         }
         wgMarker.width = marker.size.width;
         wgMarker.height = marker.size.height;
-        if (marker.rotation != 0.0)
-        {
-            wgMarker.rotation = marker.rotation;
-            wgMarker.lockRotation = true;
-        }
+        wgMarker.rotation = marker.rotation;
+        wgMarker.lockRotation = marker.lockRotation;
         if (marker.selectable)
         {
             wgMarker.isSelectable = true;
