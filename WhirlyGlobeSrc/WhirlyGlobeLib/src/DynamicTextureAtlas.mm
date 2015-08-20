f54ca13dfad09e17ac163cb2c6cfe6ec2f1ca25b
Ranen Ghosh <ranen@ranenghosh.com>
2015-08-20 13:23:28 -0400

    remove texture filtering

--- WhirlyGlobeSrc/WhirlyGlobeLib/src/DynamicTextureAtlas.mm
+++ WhirlyGlobeSrc/WhirlyGlobeLib/src/DynamicTextureAtlas.mm
@@ -126,8 +126,8 @@ bool DynamicTexture::createInGL(OpenGLMemManager *memManager)
     glBindTexture(GL_TEXTURE_2D, glId);
     CheckGLError("DynamicTexture::createInGL() glBindTexture()");
     
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
