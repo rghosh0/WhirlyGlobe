06e3e0e62ad9accbc8a5efc3bdb916de1237aefe
Ranen Ghosh <ranen@ranenghosh.com>
2016-04-10 20:14:33 -0400

    marker rotation changes

--- WhirlyGlobeSrc/WhirlyGlobe-MaplyComponent/src/MaplyBaseInteractionLayer.mm
+++ WhirlyGlobeSrc/WhirlyGlobe-MaplyComponent/src/MaplyBaseInteractionLayer.mm
@@ -775,11 +775,8 @@ public:
         }
         wgMarker.width = marker.size.width;
         wgMarker.height = marker.size.height;
-        if (marker.rotation != 0.0)
-        {
-            wgMarker.rotation = marker.rotation;
-            wgMarker.lockRotation = true;
-        }
+        wgMarker.rotation = marker.rotation;
+        wgMarker.lockRotation = marker.lockRotation;
         if (marker.selectable)
         {
             wgMarker.isSelectable = true;
